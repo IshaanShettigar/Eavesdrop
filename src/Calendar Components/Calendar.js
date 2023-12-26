e08d4e538a1759e921b5ff47b7dd605cc2d17bbe
Ishaan Shettigar <ishaanshettigar@gmail.com>
2023-12-26 08:54:26 +0530

    Calendar 100% done

--- src/Calendar Components/Calendar.js
+++ src/Calendar Components/Calendar.js
@@ -35,6 +35,24 @@ const Calendar = () => {
 
     }, [])
 
+    const gotoPrevMonth = () => {
+        let flag = false;
+        let newCalendar = []
+        calendar.slice().reverse().forEach((data) => {
+            if (data.active) {
+                flag = true;
+                newCalendar.push({ ...data, "active": false })
+            }
+            else if (flag) {
+                flag = false;
+                newCalendar.push({ ...data, "active": true })
+            }
+            else {
+                newCalendar.push({ ...data })
+            }
+        })
+        setCalendar(newCalendar.reverse())
+    }
 
     const gotoNextMonth = () => {
         let flag = false
@@ -70,7 +88,7 @@ const Calendar = () => {
                         })}
                     </span>
                     <div className="buttons">
-                        <button className="p-1">
+                        <button className="p-1" onClick={gotoPrevMonth}>
                             <svg width="1em" fill="gray" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-left-circle" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                                 <path fillRule="evenodd" d="M8.354 11.354a.5.5 0 0 0 0-.708L5.707 8l2.647-2.646a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708 0z" />
